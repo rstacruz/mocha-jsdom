9c8bb43030c12d1c2e8436aedd268789005dfa0f
epplestun <epplestun@gmail.com>
2015-10-30 16:38:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -63,6 +63,59 @@ module.exports = function (_options) {
         window.console = global.console
       }
 
+      var _attrToDataKey = function( val ) {
+        var out = val.substr( 5 );
+        return out.split( "-" ).map(function( part, inx ){
+          if ( !inx ) {
+            return part;
+          }
+          return part.charAt( 0 ).toUpperCase() + part.substr( 1 );
+        }).join( "" );
+      },
+      _datasetProxy = null,
+      _getNodeDataAttrs = function( el ){
+        var i = 0,
+            atts = el.attributes,
+            len = atts.length,
+            attr,
+            _datasetMap = [],
+            // represents el.dataset
+            proxy = {},
+            datakey;
+          for ( ; i < len; i++ ){
+            attr = atts[ i ].nodeName;
+            if ( attr.indexOf( "data-" ) === 0 ) {
+              datakey = _attrToDataKey( attr );
+              if ( typeof _datasetMap[ datakey ] !== "undefined" ) {
+                break;
+              }
+              _datasetMap[ datakey ] = atts[ i ].nodeValue;
+              (function( datakey ){
+                // every data-attr found on the element makes a getter and setter
+                Object.defineProperty( proxy, datakey, {
+                  enumerable: true,
+                  configurable: true,
+                  get: function() {
+                    return  _datasetMap[ datakey ];
+                  },
+                  set: function ( val ) {
+                    _datasetMap[ datakey ] = val;
+                    el.setAttribute( attr, val );
+                  }
+                });
+              }( datakey ));
+            }
+          }
+          return proxy;
+      };
+
+      Object.defineProperty( global.window.Element.prototype, "dataset", {
+        get: function() {
+          _datasetProxy = _datasetProxy || _getNodeDataAttrs( this );
+          return _datasetProxy;
+        }
+      });
+
       if (errors) {
         return next(getError(errors))
       }
