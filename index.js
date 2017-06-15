7c738941967eed7f02bc32917fbdf266ee43a483
Kyle Cameron <Kyle@adminITs-MacBook-Pro.local>
2017-06-15 12:29:24 -0400

    fix(index.js): Change path to require old jsdom api method
    Jsdom updated their api. '.env' function still supported, but requires different path.
    
    #27

--- index.js
+++ index.js
@@ -48,8 +48,7 @@ module.exports = function (_options) {
         'mocha-jsdom: already a browser environment, or mocha-jsdom invoked ' +
         "twice. use 'skipWindowCheck' to disable this check.")
     }
-
-    require('jsdom').env(
+      require('jsdom/lib/old-api').env(
       extend(extend({}, options), { done: done }))
 
     function done (errors, window) {
